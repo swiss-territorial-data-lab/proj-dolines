22a8b9ffabf6898ff46274c2390abcaaeb423b29
GwenaelleSa <gwenaelle.salamin@gmail.com>
2024-07-22 15:13:20 +0200

    Translate PyQGIS plugin to python

--- scripts/doline_detection.py
+++ scripts/doline_detection.py
@@ -10,8 +10,9 @@ import numpy as np
 import pandas as pd
 import rasterio as rio
 from cv2 import GaussianBlur
+from rasterstats import zonal_stats
 
-from functions.fct_misc import format_logger, get_config
+from functions.fct_misc import format_logger, get_config, polygonize_binary_raster
 
 logger = format_logger(logger)
 
@@ -29,18 +30,150 @@ DEM_DIR = cfg['dem_dir']
 
 AOI = cfg['aoi']
 
+os.chdir(WORKING_DIR)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+written_files = []
+
 logger.info('Read data...')
 
-dem_list = glob(os.path.join(DEM_DIR, '*.tif'))
-aoi_gdf = gpd.read_file(AOI)
+dem_list = glob(os.path.join(DEM_DIR, 'swissalti3d_*.tif'))
+# aoi_gdf = gpd.read_file(AOI)
+
+if len(dem_list) == 0:
+    logger.critical('No DEM files found.')
+    sys.exit(1)
 
 # ----- Generation of the DEM -----
 
-for dem_path in tqdm(dem_list, desc="Smooth DEM"):
+all_potential_sinkhols_gdf = gpd.GeoDataFrame()
+for dem_path in tqdm(dem_list, desc="Detect dolines"):
+    dem_name = os.path.basename(dem_path)
 
     with rio.open(dem_path) as src:
         dem_data = src.read(1)
         dem_meta = src.meta
 
     # Apply gaussian filter
-    filtered_dem = GaussianBlur(dem_data, ksize=33, sigmaX=5)
+    filtered_dem = GaussianBlur(dem_data, ksize=(33, 33), sigmaX=5)
+
+    dem_diff = filtered_dem - dem_data
+
+    potential_sinkholes_arr = np.where((dem_diff>1.5) & (dem_data!=dem_meta['nodata']), 1, 0)
+    potential_sinkholes_arr = potential_sinkholes_arr.astype('int16')
+
+    # Here, the mask from the 1st step should be applied. Not done.
+    # Maybe we should create a mask based on slope...
+    
+    potential_sinkholes_gdf = polygonize_binary_raster(potential_sinkholes_arr, crs=dem_meta['crs'], transform=dem_meta['transform'])
+    potential_sinkholes_gdf['corresponding_dem'] = dem_name
+
+    all_potential_sinkhols_gdf = pd.concat([all_potential_sinkhols_gdf, potential_sinkholes_gdf]) \
+                                        if not potential_sinkholes_gdf.empty else all_potential_sinkhols_gdf
+
+# TODO: uncomment and deal with dolines on several tiles
+# all_potential_sinkhols_gdf = all_potential_sinkhols_gdf.dissolve(as_index=False)
+
+logger.info('Filter depressions based on area, compactness and density...')
+
+# Filter the depressions based on area
+# Inital code only keep depressions over 300 m2. Based on the GT, we keep those over 40 m2.
+filtered_sinkholes_gdf = all_potential_sinkhols_gdf[all_potential_sinkhols_gdf.area > 40].copy()
+
+# Determine compactness
+filtered_sinkholes_gdf['compactness'] = 4 * np.pi * filtered_sinkholes_gdf.area / filtered_sinkholes_gdf.length**2
+filtered_sinkholes_gdf['type'] = ['round' if compactness > 0.33 else 'long' for compactness in filtered_sinkholes_gdf.compactness]
+
+filepath = os.path.join(OUTPUT_DIR, 'potential_sinkholes.gpkg')
+filtered_sinkholes_gdf.to_file(filepath)
+written_files.append(filepath)
+
+# Make the Vornoi diagram of the round depressions
+center_geoms = filtered_sinkholes_gdf[filtered_sinkholes_gdf['type']=='round'].geometry.centroid
+voronoi_result_polys = center_geoms.voronoi_polygons()
+
+# Find area with a high density of round depressions based on the area of vornoi polygons
+dissolved_vornoi_polys = voronoi_result_polys[voronoi_result_polys.area < 60000].union_all('coverage')
+dissolved_vornoi_polys_gs = gpd.GeoSeries([geom for geom in dissolved_vornoi_polys.geoms], crs='epsg:2056')
+assert dissolved_vornoi_polys_gs.is_valid.all(), 'Dissolved voronoi polygons are not all valid.'
+
+# Only keep large dense area and a buffer around
+large_dense_areas = dissolved_vornoi_polys_gs[dissolved_vornoi_polys_gs.area > 200000].copy()
+large_dense_areas = large_dense_areas.geometry.buffer(100)
+large_dense_areas_union = large_dense_areas.union_all()
+
+filepath = os.path.join(OUTPUT_DIR, 'large_dense_area.gpkg')
+large_dense_areas.to_file(filepath)
+written_files.append(filepath)
+
+# Only keep long depression that are within the large dense area
+long_sinkholes_gdf = filtered_sinkholes_gdf[
+    (filtered_sinkholes_gdf['type']=='long')
+    & (filtered_sinkholes_gdf.area < 9500)
+    # here we supressed a filter for area under 700 m2
+    & (filtered_sinkholes_gdf.compactness > 0.11)
+    & filtered_sinkholes_gdf.geometry.within(large_dense_areas_union)
+].copy()
+
+# Outside the dense areas, only keep very round depressions
+round_sinkholes_gdf = filtered_sinkholes_gdf[
+    (filtered_sinkholes_gdf['type']=='round')
+    & (
+        (
+            filtered_sinkholes_gdf.geometry.within(large_dense_areas_union)
+        )
+        |(
+            ~filtered_sinkholes_gdf.geometry.within(large_dense_areas_union)
+            & (filtered_sinkholes_gdf.compactness > 0.38)
+            # here we supressed a filter for area under 1000 m2
+        )
+    )
+]
+round_sinkholes_gdf.loc[:, 'type'] = [
+    'round in dense area' if geometry.within(large_dense_areas_union) else 'very round outside dense area' 
+    for geometry in round_sinkholes_gdf.geometry
+]
+
+sinkholes_gdf = pd.concat([round_sinkholes_gdf, long_sinkholes_gdf], ignore_index=True)
+sinkholes_gdf['doline_id'] = sinkholes_gdf.index
+
+logger.info('Deal with thalwegs producing false positives...')
+
+# Get an outline on the surrounding terrain
+sinkholes_gdf['buffered_outline'] = sinkholes_gdf.geometry.buffer(15).boundary
+
+sinkholes_gdf['alti_diff'] = None
+for dem_tile in tqdm(sinkholes_gdf.corresponding_dem.unique(), desc="Get lowest point of sinkhole and its buffer outline"):
+    dem_path = os.path.join(DEM_DIR, dem_tile)
+    sinkholes_on_tiles_gdf = sinkholes_gdf[sinkholes_gdf.corresponding_dem == dem_tile].copy()
+
+    # Get lowest point of sinkhole and its buffer outline
+    with rio.open(dem_path) as src:
+        sinkholes_lowest_alti = zonal_stats(sinkholes_on_tiles_gdf.geometry, src.read(1), affine=src.transform, stats='min')
+        buffer_lowest_alti = zonal_stats(sinkholes_on_tiles_gdf.buffered_outline, src.read(1), affine=src.transform, stats='min')
+
+    # Keep results in a dataframe
+    alti_dict={
+        'doline_id': sinkholes_on_tiles_gdf.doline_id,
+        'min_sinkhole': [x['min'] for x in sinkholes_lowest_alti],
+        'min_buffer_line': [x['min'] for x in buffer_lowest_alti],
+        'geometry': sinkholes_on_tiles_gdf.geometry
+    }
+    alti_gdf = gpd.GeoDataFrame(alti_dict, crs=2056)
+
+    # Detect and mark thalwegs
+    alti_gdf['alti_diff'] = alti_gdf['min_buffer_line'] - alti_gdf['min_sinkhole']
+    assert (alti_gdf.alti_diff<0).any(), 'Negative altitude difference between the sinkhole bottom and the buffer outline'
+    thalweg_ids = alti_gdf.loc[alti_gdf['alti_diff'] < 0.8, 'doline_id']
+
+    sinkholes_gdf.loc[sinkholes_gdf.doline_id.isin(thalweg_ids), 'type'] = 'thalweg'
+    sinkholes_gdf.loc[sinkholes_gdf.doline_id.isin(alti_gdf.doline_id), 'alti_diff'] = alti_gdf.alti_diff
+
+filepath = os.path.join(OUTPUT_DIR, 'sinkholes.gpkg')
+sinkholes_gdf[['doline_id', 'type', 'compactness', 'alti_diff', 'geometry']].to_file(filepath)
+written_files.append(filepath)
+
+logger.success('Done! The following files were written:')
+for file in written_files:
+    logger.success(file)
+
+logger.info(f'Done in {time() - tic:0.4f} seconds')
